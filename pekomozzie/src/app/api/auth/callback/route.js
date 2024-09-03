6b218b376a8fe3175f597af82911d40bcfd29908
Will Suto <will.suto@gmail.com>
2024-09-02 20:52:55 -0700

    wip

--- pekomozzie/src/app/api/auth/callback/route.js
+++ pekomozzie/src/app/api/auth/callback/route.js
@@ -17,17 +17,16 @@ export async function GET(request) {
       console.log("Supabase error:", error);
 
       if (!error) {
-        return NextResponse.redirect(`${origin}/dashboard`)
-        // const forwardedHost = request.headers.get('x-forwarded-host');
-        // const isLocalEnv = process.env.NODE_ENV === 'development';
+        const forwardedHost = request.headers.get('x-forwarded-host');
+        const isLocalEnv = process.env.NODE_ENV === 'development';
         
-        // if (isLocalEnv) {
-        //   return NextResponse.redirect(`${origin}${next}`);
-        // } else if (forwardedHost) {
-        //   return NextResponse.redirect(`https://${forwardedHost}${next}`);
-        // } else {
-        //   return NextResponse.redirect(`${origin}${next}`);
-        // }
+        if (isLocalEnv) {
+          return NextResponse.redirect(`${origin}${next}`);
+        } else if (forwardedHost) {
+          return NextResponse.redirect(`https://${forwardedHost}${next}`);
+        } else {
+          return NextResponse.redirect(`${origin}${next}`);
+        }
       } else {
         console.error("Error exchanging code for session:", error.message);
       }
