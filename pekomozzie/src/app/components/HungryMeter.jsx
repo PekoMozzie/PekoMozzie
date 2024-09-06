eb8066350d146f0c30e1a24c12c66f7d28d2e6b1
Max Hassen <max27183141@gmail.com>
2024-09-05 17:33:37 -0700

    began prototyping HungryMeter.jsx

---
+++ pekomozzie/src/app/components/HungryMeter.jsx
@@ -0,0 +1,9 @@
+
+
+export default function HungryMeter() {
+    return (
+        <div className="w-3/5 mx-auto bg-red-700 p-4 rounded-lg text-white text-center shadow-lg ">
+        <h3 className="text-xl mb-1">Hangryness Level</h3>
+        </div>
+    );
+}
--- pekomozzie/src/app/dashboard/page.js
+++ pekomozzie/src/app/dashboard/page.js
@@ -1,9 +1,13 @@
+import HungryMeter from "../components/HungryMeter"
 
- export default function Home () {
+
+
+export default function Home () {
   return (
     <section className ="py-24">
       <div className="container">
         <h1 className="text.3xl.font-b">PekoMozzie!</h1>
+        <HungryMeter></HungryMeter>
       </div>
     </section>
   )
