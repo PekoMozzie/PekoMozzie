e4f190957e75c297448255d0356e79c9304bef02
Will Suto <will.suto@gmail.com>
2024-09-05 22:04:42 -0700

    added webpack pathname config

--- pekomozzie/next.config.mjs
+++ pekomozzie/next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    webpack(config) {
+      config.resolve.alias['@'] = path.resolve(__dirname, 'src');
+      return config;
+    },
+  };
+  
+  module.exports = nextConfig;
+  
