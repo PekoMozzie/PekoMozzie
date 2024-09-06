19a3d7ce96ee49e8be73859e635b4eae22d46dec
Will Suto <will.suto@gmail.com>
2024-09-05 21:35:15 -0700

    wip

--- pekomozzie/src/app/dashboard/layout.jsx
+++ pekomozzie/src/app/dashboard/layout.jsx
@@ -1,4 +1,4 @@
-import SignOut from '@src/app/dashboard/components/SignOut.jsx';
+import SignOut from '@/app/dashboard/components/SignOut.jsx';
 import { metadata } from '@/app/layout.js';
 import Image from "next/image";
 
@@ -7,7 +7,7 @@ export default function NavBar({ children }) {
     <div>
       <nav className="bg-gray-500 text-white ">
           <div className="container mx-auto flex justify-between items-center py-4">
-            <a href="/dashboard"><Image src={metadata.logo} alt="PekoMozzie Logo" width="300" height="25" className="hover:"/></a>
+            <a href="/dashboard"><Image src={metadata.logo} alt="PekoMozzie Logo" width="300" height="25" className="hover:opacity-80"/></a>
             <ul className="flex space-x-4">
               <li><a href="/dashboard/history" className="hover:text-black">History</a></li>
               <li><a href="/dashboard/goal" className="hover:text-black">Goal</a></li>
