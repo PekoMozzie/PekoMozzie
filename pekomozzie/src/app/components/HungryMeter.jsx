31a6d86da4428bae90f5a25a63cd85c8d9473d60
Max Hassen <max27183141@gmail.com>
2024-09-07 11:40:28 -0700

    hangry meter prototype

--- pekomozzie/src/app/components/HungryMeter.jsx
+++ pekomozzie/src/app/components/HungryMeter.jsx
@@ -1,9 +1,37 @@
+'use client'
+
+import { useState, useEffect } from 'react'
+
 
 
 export default function HungryMeter() {
-    return (
-        <div className="w-3/5 mx-auto bg-red-700 p-4 rounded-lg text-white text-center shadow-lg ">
-        <h3 className="text-xl mb-1">Hangryness Level</h3>
-        </div>
-    );
+  const [hungerData, setHungerData] = useState([])
+  useEffect(() => {
+    const fetchHistory = async () => {
+      try {
+        const response = await fetch('/api/hunger-data');
+        const data = await response.json();
+        setHungerData(data);
+      } catch (error) {
+        console.error('Error fetching job history', error);
+      }
+    }
+    fetchHistory().then(() => {
+      setHungerData(hungerData);
+    })
+  }, [])
+  useEffect(() => {
+    
+  }, [hungerData])
+
+  function hungerLevel() {
+    return Math.min((hungerData.jobApps? hungerData.jobApps.length : 0 / hungerData.weekly_quota? hungerData.weekly_quota : 1)*100, 100);
+  }
+  useEffect(() => {
+  }, [hungerData])
+  return (
+    <div className='mx-auto w-3/5 rounded-lg bg-red-700 p-4 text-center text-white shadow-lg'>
+      <h3 className='mb-1 text-xl'>Hangryness Level {hungerLevel()} </h3>
+    </div>
+  )
 }
